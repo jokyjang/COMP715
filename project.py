5880bf614631185ea1a16567f0610b10aa6b942f
gopidirajesh <rajesh@cs.unc.edu>
2014-04-19 00:23:36 -0400

    glyphs and uncertainity working

--- project.py
+++ project.py
@@ -51,19 +51,24 @@ class MainWindow(QtGui.QWidget):
         self.vl = QtGui.QVBoxLayout()
         self.frame.setLayout(self.vl)
 
+        self.widthL = 1100
+        self.heightL = 600
+
         splitter = QtGui.QSplitter(QtCore.Qt.Horizontal)
         splitter.addWidget(left)
         splitter.addWidget(self.frame)
+        splitter.setSizes([225, self.widthL-225])
 
+        print "fuck you#", self.widthL
         vbox = QtGui.QHBoxLayout()
         vbox.addWidget(splitter)
         self.setLayout(vbox)
         QtGui.QApplication.setStyle(QtGui.QStyleFactory.create('Cleanlooks'))
         
-        self.setGeometry(1200, 800, 800, 600)
+        self.setGeometry(0, 0, self.widthL, self.heightL)
         
         self.setWindowTitle('Visualization in Science')
-        #self.show()
+        self.show()
     
     def get_vtr_file(self):
         
@@ -80,26 +85,35 @@ class MainWindow(QtGui.QWidget):
         self.draw_graph_from_file(inputFileName)
         
     def draw_graph(self):
-        print "rajesh# ", rajesh
         self.year = int(self.yearLe.text())
         self.month = int(self.monthLe.text())
         inputFileName = self.get_vtr_file()
         self.draw_graph_from_file(inputFileName)
 
-    def slice_data(self, dataset, arrayName, lower, upper):
+    def slice_data(self, dataset, arrayName, lower, upper, between,
+            sliceByLower, sliceByUpper):
 
-        dataset.GetPointData().SetActiveScalars(arrayName)
         thresholdFilter = vtkThresholdPoints()
-        thresholdFilter.ThresholdBetween(lower, upper)
+        dataset.GetPointData().SetActiveScalars(arrayName)
+
+        if between:
+            thresholdFilter.ThresholdBetween(lower, upper)
+        elif sliceByUpper:
+           thresholdFilter.ThresholdByUpper(upper) 
+        else:
+            thresholdFilter.ThresholdByLower(lower)
+
         thresholdFilter.SetInput(dataset)
         thresholdFilter.Update()
 
+        #gFilter = vtkGeometryFilter()
+        #gFilter.SetInput(thresholdFilter.GetOutput())
+        #gFilter.Update()
         return thresholdFilter.GetOutput()
+        #return gFilter.GetOutput()
 
     def get_phase_color_map(self, dataset):
         
-        print 'hi'
-
         transFunction = vtkDiscretizableColorTransferFunction()
         transFunction.SetNumberOfValues(8)
         transFunction.DiscretizeOn()
@@ -115,6 +129,13 @@ class MainWindow(QtGui.QWidget):
         scalarBar = vtkScalarBarActor()
         #scalarBar.SetLookupTable(colorLookupTable)
         scalarBar.SetLookupTable(transFunction)
+        scalarBar.SetDisplayPosition(self.widthL * 0.2, 20)
+        scalarBar.SetWidth(0.5)
+        scalarBar.SetHeight(0.08)
+        # Do not stretch beyond the default size
+        scalarBar.SetMaximumWidthInPixels(scalarBar.GetWidth() * self.widthL)
+        scalarBar.SetMaximumHeightInPixels(scalarBar.GetHeight() * self.heightL)
+        scalarBar.SetOrientationToHorizontal()
         self.ren.AddActor2D(scalarBar)
         colors = vtkUnsignedCharArray()
         colors.SetNumberOfComponents(3)
@@ -131,9 +152,9 @@ class MainWindow(QtGui.QWidget):
                 for j in range(3):
                     color[j] = int(255.0 * dcolor[j])
             else:
-                color[0] = 0
-                color[1] = 0
-                color[2] = 255
+                color[0] = 82 
+                color[1] = 87
+                color[2] = 110
 
             colors.InsertNextTupleValue(color)
 
@@ -144,31 +165,77 @@ class MainWindow(QtGui.QWidget):
 
         return (255-temp)
 
-    def get_amp_color_map(self, dataset):
+    def get_uncertainity_plan (self, dataset, low, high):
 
-        '''
-        transFunction = vtkDiscretizableColorTransferFunction()
-        transFunction.SetNumberOfValues(8)
-        transFunction.DiscretizeOn()
-        transFunction.EnableOpacityMappingOn()
-        transFunction.AddRGBPoint(-1.0, 1, 1, 1)
-        #transFunction.AddRGBPoint(-0.6, 0.902, 0.902, 0)
-        #transFunction.AddRGBPoint(-0.757979, 0.9294, 0.9294, 0.2862)
-        #transFunction.AddRGBPoint(-0.488299, 236, 236, 59)
-        #transFunction.AddRGBPoint(0, 230, 0, 0)
-        #transFunction.AddRGBPoint(0.244677, 164, 0 , 0)
-        #transFunction.AddRGBPoint(0.2, 0.902,0,0)
-        transFunction.AddRGBPoint(1.0, 1, 1, 1)
-        '''
+        clone = dataset
+        #clone = self.slice_data(dataset, self.amplitude, low, high, True, False,
+        #       False)
+        amplData = clone.GetPointData().GetArray(self.amplitude)
+        clone.GetPointData().SetActiveScalars(self.amplitude)
+
+        colors1 = self.get_amp_color_map(amplData, 0.2, 1)
+        clone.GetPointData().SetScalars(colors1)
+
+        ampMapper = vtkDataSetMapper()
+        ampMapper.SetInput(clone)
+        ampMapper.ScalarVisibilityOn()
+
+        actor2 = vtkActor()
+        actor2.SetMapper(ampMapper)
+        actor2.GetProperty().SetOpacity(0.97);
+
+        return actor2
+
+    def get_glyph_plan (self, dataset, low, high):
+
+        #lowerHalf = self.clone_data(dataset, self.amplitude)
+
+        clone = dataset
+        clone = self.slice_data(dataset, self.amplitude, low, high, True, False,
+               False)
+        amplData = clone.GetPointData().GetArray(self.amplitude)
+        clone.GetPointData().SetActiveScalars(self.amplitude)
+
+        glyphs = vtkGlyphSource2D()
+        glyphs.SetGlyphTypeToTriangle()
+        glyphs.FilledOff()
+        glyphs.SetColor(255,255,0)
+        glyphs.Update()
+
+        glyph = vtkGlyph2D()
+        glyph.SetInput(clone)
+        glyph.SetScaleModeToScaleByScalar()
+        glyph.SetScaleFactor(5)
+        glyph.SetSourceConnection(glyphs.GetOutputPort())
+        #glyph.SetColorModeToColorByVector()
+        glyph.SetRange(low, high)
+        glyph.Update()
+
+
+        print "color mode# ", glyph.GetColorModeAsString()
+
+        lut =  vtkLookupTable()
+        lut.SetNumberOfTableValues(2);
+        lut.SetRange(0.0,1.0);
+        lut.SetTableValue( 0, 1.0, 0.0, 0.0);
+        lut.SetTableValue( 1, 1.0, 0.0, 0.0);
+        #lut.IndexedLookupOn ()
+        lut.Build()
+
+        #glyphMapper = vtkGlyph3DMapper()
+        glyphMapper = vtkPolyDataMapper()
+        glyphMapper.SetInputConnection(glyph.GetOutputPort())
+        glyphMapper.SetLookupTable(lut) 
+        actor2 = vtkActor()
+        actor2.SetMapper(glyphMapper)
+
+        return actor2
+
+    def get_amp_color_map(self, dataset, low, high):
+
         (minv, maxv) = dataset.GetRange()
-        self.amp_min = minv
-        self.amp_max = maxv
-       
-        print "minv#, maxv#",  minv, maxv       
-        #scalarBar = vtkScalarBarActor()
-        #scalarBar.SetLookupTable(colorLookupTable)
-        #scalarBar.SetLookupTable(transFunction)
-        #self.ren.AddActor2D(scalarBar)
+ 
+        print "minv#, maxv#",  low, high       
         colors = vtkUnsignedCharArray()
         colors.SetNumberOfComponents(4)
         colors.SetName("Colors")
@@ -176,24 +243,32 @@ class MainWindow(QtGui.QWidget):
         for i in range(dataset.GetNumberOfTuples()):
             p = dataset.GetValue(i)
             color = [0 for i in range(4)]
-            color[0] = 255
-            color[1] = 255
-            color[2] = 255
-            color[3] = 255
-            if p >= -1:  
-                color[3] = self.get_opacity_value(p, 16)
+            if p >= low and p <= high:  
+                for j in range(4):
+                    color[j] = 155
+                color[3] = 0
+            elif p >= -1 and p < low:
+                color[0] = 55
+                color[1] = 55
+                color[2] = 55
+                color[3] = 180
+            else:
+                color[0] = 82
+                color[1] = 87
+                color[2] = 110
+                color[3] = 255
 
             colors.InsertNextTupleValue(color)
 
         return colors
 
-    def clone_data(self, dataset):
+    def clone_data(self, dataset, target):
 
         calc = vtkArrayCalculator()
         calc.SetInput(dataset)
-        calc.AddScalarArrayName("ampanomfil")
-        calc.SetFunction("ampanomfil");
-        calc.SetResultArrayName("clone");
+        calc.AddScalarArrayName(target)
+        calc.SetFunction(target);
+        calc.SetResultArrayName(target);
         calc.Update();
         
         return calc.GetOutput()
@@ -210,9 +285,11 @@ class MainWindow(QtGui.QWidget):
         #typecasting to a more general class vtkDataSet
         dataSet = vtkDataSet.SafeDownCast(reader.GetOutput())
 
-        clone = self.clone_data(dataSet)
-       
-        #dataSet = self.slice_data(dataSet, 'phaseanomfil', -1, 1)
+        clone = self.clone_data(dataSet, self.amplitude)
+
+        #dataSet = self.slice_data(dataSet, 'phaseanomfil', -1, 1, True, False,
+        #        False)
+        print "#debug"
         pd = dataSet.GetPointData()
         numOfArrays = pd.GetNumberOfArrays()
         sstData = pd.GetArray('phaseanomfil')
@@ -221,57 +298,42 @@ class MainWindow(QtGui.QWidget):
             print pd.GetArrayName(i)
         
         print sstData.GetSize()
-        print sstData.GetValue(16000)
         print sstData.GetNumberOfComponents()
-        print dataSet.GetPoint(16000)
         (minz, maxz) = sstData.GetRange()
         #print minz, maxz
        
         colors = self.get_phase_color_map(sstData)
         dataSet.GetPointData().SetScalars(colors)
-        
         # Create a mapper
         mapper = vtkDataSetMapper()
         #mapper.SetInputConnection(reader.GetOutputPort())
         mapper.SetInput(dataSet)
         mapper.ScalarVisibilityOn()
-        #mapper.SetInputData(sstData)
         
         # Create an actor
         actor = vtkActor()
         actor.SetMapper(mapper)
- 
+         
         #amplitude part starts here
+        #actor2 = self.get_glyph_plane(clone, 0, 1)
 
-        clone = self.slice_data(clone, 'ampanomfil', -1, 1)
-        amplData = clone.GetPointData().GetArray('ampanomfil')
-
-
-        colors1 = self.get_amp_color_map(amplData)
-        clone.GetPointData().SetScalars(colors1)
-
-        ampMapper = vtkDataSetMapper()
-        ampMapper.SetInput(clone)
-        ampMapper.ScalarVisibilityOn()
-
-        actor2 = vtkActor()
-        actor2.SetMapper(ampMapper)
-        #actor2.GetProperty().SetOpacity(0.995);
-
+        actor2 = self.get_uncertainity_plan(clone, 0.3, 1)
         self.ren.AddActor(actor)
         self.ren.AddActor(actor2)
         self.ren.ResetCamera()
-        #self.show()
+        self.show()
         self.iren.Initialize()
         
     def initVTK(self):
         
         self.vtkWidget = QVTKRenderWindowInteractor(self.frame)
         self.vl.addWidget(self.vtkWidget)
- 
+
+        self.amplitude = "ampanomfil"
         self.amp_max = 0
         self.amp_min = 0
         self.ren = vtkRenderer()
+        self.ren.SetBackground(0.32, 0.34, 0.43)
         self.vtkWidget.GetRenderWindow().AddRenderer(self.ren)
         self.iren = self.vtkWidget.GetRenderWindow().GetInteractor()
 
--- test.py
+++ test.py
@@ -1,22 +1,53 @@
-from vtk import *
-#import sys
-
+#!/usr/bin/env python
+ 
+import sys
+import vtk
+from PyQt4 import QtCore, QtGui
+from vtk.qt4.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+ 
+class MainWindow(QtGui.QMainWindow):
+ 
+    def __init__(self, parent = None):
+        QtGui.QMainWindow.__init__(self, parent)
+ 
+        self.frame = QtGui.QFrame()
+ 
+        self.vl = QtGui.QVBoxLayout()
+        self.vtkWidget = QVTKRenderWindowInteractor(self.frame)
+        self.vl.addWidget(self.vtkWidget)
+ 
+        self.ren = vtk.vtkRenderer()
+        self.vtkWidget.GetRenderWindow().AddRenderer(self.ren)
+        self.iren = self.vtkWidget.GetRenderWindow().GetInteractor()
+ 
+        # Create source
+        source = vtk.vtkSphereSource()
+        source.SetCenter(0, 0, 0)
+        source.SetRadius(5.0)
+ 
+        # Create a mapper
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInputConnection(source.GetOutputPort())
+ 
+        # Create an actor
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
+ 
+        self.ren.AddActor(actor)
+ 
+        self.ren.ResetCamera()
+ 
+        self.frame.setLayout(self.vl)
+        self.setCentralWidget(self.frame)
+ 
+        self.show()
+        self.iren.Initialize()
+ 
+ 
 if __name__ == "__main__":
-    cylinder = vtkCylinderSource()
-    cylinder.SetResolution(128)
-    mapper = vtkPolyDataMapper()
-    mapper.SetInputConnection(cylinder.GetOutputPort())
-    actor = vtkActor()
-    actor.SetMapper(mapper)
-    
-    ren1 = vtkRenderer()
-    renWin = vtkRenderWindow()
-    renWin.AddRenderer(ren1)
-    iren = vtkRenderWindowInteractor()
-    iren.SetRenderWindow(renWin)
-    
-    ren1.AddActor(actor)
-    #ren1.SetBackground(.1, .2, .3) # Background color dark blue
-    
-    renWin.Render()
-    iren.Start()
+ 
+    app = QtGui.QApplication(sys.argv)
+ 
+    window = MainWindow()
+ 
+    sys.exit(app.exec_())
