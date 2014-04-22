247e59ccc97d46546a2d497ff6c27a2b800e7ce3
gopidirajesh <rajesh@cs.unc.edu>
2014-04-22 13:40:21 -0400

    Final working code

--- project.py
+++ project.py
@@ -111,7 +111,7 @@ class MainWindow(QtGui.QWidget):
         return thresholdFilter.GetOutput()
         #return gFilter.GetOutput()
 
-    def get_lut(self, target, minv, maxv, renderer):
+    def get_lut(self, minv, maxv, renderer):
       
         no_of_steps = 8
         dim = 3
@@ -124,12 +124,12 @@ class MainWindow(QtGui.QWidget):
         colors[1][0] = 0.40
         colors[1][1] = 0.54
         colors[1][2] = 0.93
-        colors[2][0] = 0.43
-        colors[2][1] = 0.56
-        colors[2][2] = 0.95
-        colors[3][0] = 0.5
-        colors[3][1] = 0.63
-        colors[3][2] = 0.98
+        colors[2][0] = 0.58
+        colors[2][1] = 0.71
+        colors[2][2] = 1
+        colors[3][0] = 0.6
+        colors[3][1] = 0.72
+        colors[3][2] = 1
         colors[4][0] = 0.87
         colors[4][1] = 0.87
         colors[4][2] = 0.87
@@ -147,22 +147,25 @@ class MainWindow(QtGui.QWidget):
         colors[8][2] = 0.15
 
         transFunction = vtkDiscretizableColorTransferFunction()
-        transFunction.SetNumberOfValues(no_of_steps+1)
         transFunction.DiscretizeOn()
+        transFunction.SetNumberOfValues(no_of_steps)
 
         for i in range(0, no_of_steps+1):
-            transFunction.addRGBPoint(minv + i * step, colors[i][0],
+            transFunction.AddRGBPoint(minv + i * step, colors[i][0],
                     colors[i][1], colors[i][2])
+            print "step value# ", minv + i * step
 
+        '''
         scalarBarTransFunction = vtkDiscretizableColorTransferFunction()
         scalarBarTransFunction.DeepCopy(transFunction)
-        scalarBarTransFunction.SetNumberOfValues(no_of_steps)
+        scalarBarTransFunction.SetNumberOfValues(no_of_steps+1)
+        '''
 
         #continents in deep purple color
-        transFunction.AddRGBPoint(-999, 0.56, 0.27, 0.52)
+        #transFunction.AddRGBPoint(-999, 0.56, 0.27, 0.52)
 
         scalarBar = vtkScalarBarActor()
-        scalarBar.SetLookupTable(scalarBarTransFunction)
+        scalarBar.SetLookupTable(transFunction)
         scalarBar.SetDisplayPosition(self.widthL * 0.2, 20)
         scalarBar.SetWidth(0.5)
         scalarBar.SetHeight(0.08)
@@ -174,102 +177,50 @@ class MainWindow(QtGui.QWidget):
 
         return transFunction
 
-    def get_phase_color_map(self, dataset):
-
-        transFunction  = self.get_lut()
-        scalarBar = vtkScalarBarActor()
-        #scalarBar.SetLookupTable(colorLookupTable)
-        scalarBar.SetLookupTable(transFunction)
-        scalarBar.SetDisplayPosition(self.widthL * 0.2, 20)
-        scalarBar.SetWidth(0.5)
-        scalarBar.SetHeight(0.08)
-        # Do not stretch beyond the default size
-        scalarBar.SetMaximumWidthInPixels(scalarBar.GetWidth() * self.widthL)
-        scalarBar.SetMaximumHeightInPixels(scalarBar.GetHeight() * self.heightL)
-        scalarBar.SetOrientationToHorizontal()
-        self.ren.AddActor2D(scalarBar)
-        colors = vtkUnsignedCharArray()
-        colors.SetNumberOfComponents(3)
-        colors.SetName("Colors")
-       
-        print '#no of tuples:', dataset.GetNumberOfTuples()
-        for i in range(dataset.GetNumberOfTuples()):
-            p = dataset.GetValue(i)
-            color = [0 for i in range(3)]
-            if p >= -1:  
-                dcolor = [0 for i in range(3)]
-                #colorLookupTable.GetColor(p, dcolor)
-                transFunction.GetColor(p, dcolor)
-                for j in range(3):
-                    color[j] = int(255.0 * dcolor[j])
-            else:
-                color[0] = 82 
-                color[1] = 87
-                color[2] = 110
-
-            colors.InsertNextTupleValue(color)
-
-        return colors
-
-    def get_opacity_value(self, value, no_of_bands):
-        temp = (((value - self.amp_min) // (self.amp_max-self.amp_min)) + 1 ) * (255/no_of_bands)
-
-        return (255-temp)
-
-    def get_uncertainity_plan (self, dataset, low, high):
+    def get_uncertainity_plane (self, dataset, target, low, high):
 
         clone = dataset
-        #clone = self.slice_data(dataset, self.amplitude, low, high, True, False,
-        #       False)
-        amplData = clone.GetPointData().GetArray(self.amplitude)
-        clone.GetPointData().SetActiveScalars(self.amplitude)
+        amplData = clone.GetPointData().GetArray(target)
+        clone.GetPointData().SetActiveScalars(target)
 
-        colors1 = self.get_amp_color_map(amplData, 0.2, 1)
-        clone.GetPointData().SetScalars(colors1)
+        colors = self.get_amp_color_map(amplData, low, high)
+        clone.GetPointData().SetScalars(colors)
 
         ampMapper = vtkDataSetMapper()
         ampMapper.SetInput(clone)
         ampMapper.ScalarVisibilityOn()
 
-        actor2 = vtkActor()
-        actor2.SetMapper(ampMapper)
-        actor2.GetProperty().SetOpacity(0.97);
+        actor = vtkActor()
+        actor.SetMapper(ampMapper)
+        actor.GetProperty().SetOpacity(0.97);
 
-        return actor2
+        return actor
 
-    def get_glyph_plane (self, dataset, low, high):
+    def get_glyph_plane (self, clone, target, low, high):
 
-        #lowerHalf = self.clone_data(dataset, self.amplitude)
-
-        clone = dataset
-        clone = self.slice_data(dataset, self.amplitude, low, high, True, False,
-               False)
-        amplData = clone.GetPointData().GetArray(self.amplitude)
-        clone.GetPointData().SetActiveScalars(self.amplitude)
+        amplData = clone.GetPointData().GetArray(target)
+        clone.GetPointData().SetActiveScalars(target)
 
         glyphs = vtkGlyphSource2D()
         glyphs.SetGlyphTypeToTriangle()
         glyphs.FilledOff()
-        glyphs.SetColor(255,255,0)
+        glyphs.SetColor(0, 255, 255)
         glyphs.Update()
 
         glyph = vtkGlyph2D()
         glyph.SetInput(clone)
         glyph.SetScaleModeToScaleByScalar()
-        glyph.SetScaleFactor(5)
+        glyph.SetScaleFactor(2)
         glyph.SetSourceConnection(glyphs.GetOutputPort())
         #glyph.SetColorModeToColorByVector()
         glyph.SetRange(low, high)
         glyph.Update()
 
-
-        print "color mode# ", glyph.GetColorModeAsString()
-
         lut =  vtkLookupTable()
         lut.SetNumberOfTableValues(2);
         lut.SetRange(0.0,1.0);
-        lut.SetTableValue( 0, 1.0, 0.0, 0.0);
-        lut.SetTableValue( 1, 1.0, 0.0, 0.0);
+        lut.SetTableValue( 0, 0, 1.0, 1.0);
+        lut.SetTableValue( 1, 0.0, 1.0, 1.0);
         #lut.IndexedLookupOn ()
         lut.Build()
 
@@ -282,6 +233,7 @@ class MainWindow(QtGui.QWidget):
 
         return actor2
 
+    #this is used for the overlay
     def get_amp_color_map(self, dataset, low, high):
 
         (minv, maxv) = dataset.GetRange()
@@ -302,11 +254,11 @@ class MainWindow(QtGui.QWidget):
                 color[0] = 55
                 color[1] = 55
                 color[2] = 55
-                color[3] = 180
+                color[3] = 200
             else:
-                color[0] = 82
-                color[1] = 87
-                color[2] = 110
+                color[0] = 142
+                color[1] = 69
+                color[2] = 133
                 color[3] = 255
 
             colors.InsertNextTupleValue(color)
@@ -326,23 +278,137 @@ class MainWindow(QtGui.QWidget):
 
     # Here it is assumed that the dataset contains only valid entries and
     # all the invalid entries are marked with value -999
-    def get_correlation_uncertainity_design(self, dataset, target, renderer):
+    def get_correlation_uncertainity_design(self, dataset, target, renderer,
+            low, high):
 
         clone = self.clone_data (dataset, target)
 
-        pd = dataSet.GetPointData()
+        #to get the minimum in the dataset
+        clone2 = self.clone_data (dataset, target)
+        clone2 = self.slice_data(clone2, target, -1, 1, True, False, False)
+        pointData = clone2.GetPointData()
+        ampl = pointData.GetArray(target)
+
+        pd = dataset.GetPointData()
         amplitude = pd.GetArray(target)
         #print sstData.__class__
         #for i in range(numOfArrays):
-     
-        (minv, maxv) = amplitude.GetRange()
-        lut = self.get_lut(target, minv, maxv, renderer) 
-        #colors = self.get_phase_color_map(sstData)
-        dataSet.GetPointData().SetActiveScalars('phaseanomfil')
-        #dataSet.GetPointData().SetScalars(colors)
-        # Create a mapper
+   
+        (minv, maxv) = ampl.GetRange()
+        if minv == -999:
+            minv = -1
+
+        print "amplitude min# max#", minv, maxv
+        lut = self.get_lut(minv, maxv, renderer) 
+        dataset.GetPointData().SetActiveScalars(target)
+        
         mapper = vtkDataSetMapper()
-        #mapper.SetInputConnection(reader.GetOutputPort())
+        mapper.SetInput(dataset)
+        mapper.InterpolateScalarsBeforeMappingOn()
+        mapper.ScalarVisibilityOn()
+        mapper.SetColorModeToMapScalars()
+        mapper.SetLookupTable(lut)
+        
+        # Create an actor
+        actor = vtkActor()
+        actor.SetMapper(mapper)
+        actor.SetScale(8.0, 8.0, 0.0)
+         
+        actor2 = self.get_uncertainity_plane(clone, target, low, high)
+        actor2.SetScale(8.0, 8.0, 0.0)
+        renderer.AddActor2D(actor)
+        renderer.AddActor2D(actor2)
+        renderer.ResetCamera()
+
+        return renderer
+
+    def get_phase_color_scheme (self, minv, maxv, renderer):
+      
+        no_of_steps = 8
+        dim = 3
+        step = (maxv-minv)/no_of_steps
+
+        colors = [[0 for k in xrange(dim)] for j in xrange(no_of_steps+1)]
+        colors[8][0] = 0
+        colors[8][1] = 0
+        colors[8][2] = 0
+        colors[7][0] = 0.28
+        colors[7][1] = 0.0
+        colors[7][2] = 0.0
+        colors[6][0] = 0.50
+        colors[6][1] = 0.0
+        colors[6][2] = 0.0
+        colors[5][0] = 0.64
+        colors[5][1] = 0
+        colors[5][2] = 0
+        colors[4][0] = 0.90
+        colors[4][1] = 0.047
+        colors[4][2] = 0.0
+        colors[3][0] = 0.90
+        colors[3][1] = 0.36
+        colors[3][2] = 0.0
+        colors[2][0] = 0.90
+        colors[2][1] = 0.71
+        colors[2][2] = 0
+        colors[1][0] = 0.97
+        colors[1][1] = 0.97
+        colors[1][2] = 0.67
+        colors[0][0] = 1
+        colors[0][1] = 1
+        colors[0][2] = 1
+
+        transFunction = vtkDiscretizableColorTransferFunction()
+        transFunction.SetNumberOfValues(no_of_steps)
+        transFunction.DiscretizeOn()
+
+        for i in range(0, no_of_steps+1):
+            transFunction.AddRGBPoint(minv + i * step, colors[i][0],
+                    colors[i][1], colors[i][2])
+            print "min value# ", minv + i * step
+
+        scalarBarTransFunction = vtkDiscretizableColorTransferFunction()
+        scalarBarTransFunction.SetNumberOfValues(no_of_steps)
+        scalarBarTransFunction.DiscretizeOn()
+
+        for i in range(0, no_of_steps+1):
+            scalarBarTransFunction.AddRGBPoint(minv + i * step, colors[i][0],
+                    colors[i][1], colors[i][2])
+
+        #continents in deep purple color
+        #transFunction.AddRGBPoint(-2, 0.2, 0.2, 0.2)
+
+        scalarBar = vtkScalarBarActor()
+        scalarBar.SetLookupTable(scalarBarTransFunction)
+        scalarBar.SetDisplayPosition(self.widthL * 0.2, 20)
+        scalarBar.SetWidth(0.5)
+        scalarBar.SetHeight(0.08)
+        # Do not stretch beyond the default size
+        scalarBar.SetMaximumWidthInPixels(scalarBar.GetWidth() * self.widthL)
+        scalarBar.SetMaximumHeightInPixels(scalarBar.GetHeight() * self.heightL)
+        scalarBar.SetOrientationToHorizontal()
+        renderer.AddActor2D(scalarBar)
+
+        return transFunction
+
+    def get_correlation_phase_map(self, dataSet, phaseArray, ampArray, renderer, low,
+            high):
+
+        clone = self.clone_data(dataSet, ampArray)
+       
+        pd = dataSet.GetPointData()
+        phaseData = pd.GetArray(phaseArray)
+
+        clone2 = self.clone_data (dataSet, phaseArray)
+        clone2 = self.slice_data(clone2, phaseArray, -1, 1, True, False, False)
+        pointData = clone2.GetPointData()
+        data = pointData.GetArray(phaseArray)
+       
+        (minv, maxv) = data.GetRange() 
+        print "min# max#", minv, maxv
+        lut = self.get_phase_color_scheme(minv, maxv, renderer)
+        dataSet.GetPointData().SetActiveScalars(phaseArray)
+        
+        mapper = vtkDataSetMapper()
         mapper.SetInput(dataSet)
         mapper.InterpolateScalarsBeforeMappingOn()
         mapper.ScalarVisibilityOn()
@@ -352,22 +418,16 @@ class MainWindow(QtGui.QWidget):
         # Create an actor
         actor = vtkActor()
         actor.SetMapper(mapper)
-        actor.SetScale(8.0, 8.0, 8.0)
-         
-        #amplitude part starts here
-        #actor2 = self.get_glyph_plane(clone, 0, 1)
-        #actor2.SetScale(8.0, 8.0, 8.0)
+        actor.SetScale(8.0, 8.0, 0.0)
+ 
+        clone = self.slice_data(clone, ampArray, -1, 1, True, False, False)
 
-        actor2 = self.get_uncertainity_plan(clone, 0.3, 1)
-        actor2.SetScale(8.0, 8.0, 8.0)
-        self.ren.AddActor2D(actor)
-        self.ren.AddActor2D(actor2)
-        self.ren.ResetCamera()
-        self.show()
-        self.iren.Initialize()
-        
+        actor2  = self.get_glyph_plane(clone, ampArray, 0.6, 1)        
+        actor2.SetScale(8.0, 8.0, 0.0)
+        renderer.AddActor(actor)
+        renderer.AddActor(actor2) 
+        return renderer
 
-        
     def draw_graph_from_file(self, inputFileName):
         
         print inputFileName
@@ -380,40 +440,14 @@ class MainWindow(QtGui.QWidget):
         #typecasting to a more general class vtkDataSet
         dataSet = vtkDataSet.SafeDownCast(reader.GetOutput())
 
-        clone = self.clone_data(dataSet, self.amplitude)
+        #1st Goal
+        #self.ren = self.get_correlation_uncertainity_design(dataSet, "ampanomfil",
+        #        self.ren, 0.6, 1)
+        #dataSet.GetPointData().SetActiveScalars('phaseanomfil')
 
-        pd = dataSet.GetPointData()
-        numOfArrays = pd.GetNumberOfArrays()
-        sstData = pd.GetArray('phaseanomfil')
-        #print sstData.__class__
-        #for i in range(numOfArrays):
-      
-        lut = self.get_lut() 
-        #colors = self.get_phase_color_map(sstData)
-        dataSet.GetPointData().SetActiveScalars('phaseanomfil')
-        #dataSet.GetPointData().SetScalars(colors)
-        # Create a mapper
-        mapper = vtkDataSetMapper()
-        #mapper.SetInputConnection(reader.GetOutputPort())
-        mapper.SetInput(dataSet)
-        mapper.InterpolateScalarsBeforeMappingOn()
-        mapper.ScalarVisibilityOn()
-        mapper.SetColorModeToMapScalars()
-        mapper.SetLookupTable(lut)
-        
-        # Create an actor
-        actor = vtkActor()
-        actor.SetMapper(mapper)
-        actor.SetScale(8.0, 8.0, 8.0)
-         
-        #amplitude part starts here
-        #actor2 = self.get_glyph_plane(clone, 0, 1)
-        #actor2.SetScale(8.0, 8.0, 8.0)
+        self.ren = self.get_correlation_phase_map(dataSet,'phaseanomfil',
+                "ampanomfil", self.ren, 0.4, 1)
 
-        actor2 = self.get_uncertainity_plan(clone, 0.3, 1)
-        actor2.SetScale(8.0, 8.0, 8.0)
-        self.ren.AddActor2D(actor)
-        self.ren.AddActor2D(actor2)
         self.ren.ResetCamera()
         self.show()
         self.iren.Initialize()
